5cc02558d7dd080a57a1c13c72dff847f4c61d8f
Thibaut Watrisse <108615936+ShonenNoSeishin@users.noreply.github.com>
2024-04-10 16:28:56 +0200

    Commit for fun

--- fnm_watui_project/fnm_watui_project/wsgi.py
+++ fnm_watui_project/fnm_watui_project/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
+# "6b66r/wYJA0Eu0hAOpHTSJMlTqTqmJnM/iJx/tT4Y2w=" --> AES
 import os
 
 from django.core.wsgi import get_wsgi_application
